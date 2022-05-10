2281565848e6ad3532083247c36ebbdb75aa3cd1
ericc <ericcartman2700@gmail.com>
2022-05-10 02:19:04 +0200

    Add project files.

---
+++ 1.e Projekt/Helpers/JsonFileReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1.e_Projekt.Helpers
+{
+    public class JsonFileReader
+    {
+
+
+    }
+}
