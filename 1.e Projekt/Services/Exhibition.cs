7314ee32b8590c8900c1eee4ca2cbc7e67291923
ericc <ericcartman2700@gmail.com>
2022-05-12 06:10:24 +0200

    Har tilføjet nogle metoder, og lydfiler.

--- 1.e Projekt/Services/Exhibition.cs
+++ 1.e Projekt/Services/Exhibition.cs
@@ -8,9 +8,61 @@ namespace _1.e_Projekt.Services
 {
     public class Exhibition
     {
-        public int ExhibitionId { get; private set; }
+        private Dictionary<int, Exhibition> Exhibitions { get; set; }
+        public int ExhibitionId { get; set; }
         public string ExhibitionName { get; set; }
         //public Button Button{ get; set; }
 
+        public Exhibition()
+        {
+
+            {
+                Exhibitions = new Dictionary<int, Exhibition>();
+                Exhibitions.Add(1, new Exhibition() { ExhibitionId = 1, ExhibitionName = "Rockens opstart" });
+                Exhibitions.Add(2, new Exhibition() { ExhibitionId = 2, ExhibitionName = "Musik før 2.Verdenskrig" });
+                Exhibitions.Add(3, new Exhibition() { ExhibitionId = 3, ExhibitionName = "Protestmusikken i 60'erne" });
+                Exhibitions.Add(4, new Exhibition() { ExhibitionId = 4, ExhibitionName = "Stoffers indflydelse på musik " });
+
+
+            }
+
+        }
+
+        // er ikk helt sikker på hvad at dette gør
+        //public Dictionary<int, Exhibition> GetExhibitions()
+        //{
+        //    return Exhibitions;
+        //}
+
+        public Exhibition GetExhibition(int id)
+        {
+            return Exhibitions[id];
+        }
+
+        public void DeleteExhibition ()
+        {
+            Exhibitions.Remove(ExhibitionId);
+        }
+
+        public void AddExhibition(Exhibition ex)
+        {
+            if(!Exhibitions.Keys.Contains(ExhibitionId))
+            {
+                Exhibitions.Add(ExhibitionId, ex);
+            }
+        }
+        public void UpdateExhibition ()
+        {
+            foreach (var id in Exhibitions)
+            {
+                if(id.Equals(ExhibitionId))
+                {
+                    ExhibitionId = ExhibitionId;
+                    ExhibitionName = ExhibitionName;
+                }
+            }
+        }
+
     }
 }
+
--- 1.e Projekt/Services/Presentation.cs
+++ 1.e Projekt/Services/Presentation.cs
@@ -6,11 +6,53 @@ using Xamarin.Forms;
 
 namespace _1.e_Projekt.Services
 {
-    public class Presentation
+    public class Presentation: Exhibition
     {
-        public string Name { get;  private set; }
+        private Dictionary<int, Presentation> Presentations { get; set; }
+        private Dictionary<int, Exhibition> Exhibitions { get; set; }
+        public string PresentationName { get;  private set; }
+        public int PresentationId { get; private set; }
+
         //ikke sikker på at jeg kommer til at bruge denne prop. Hvis så læs mere om Xamarin.Forms.Husk Xamarin.Forms bruges som regel til mobil dev.
 
         //public Button MyProperty { get; set; }
+
+        public Presentation()
+        {
+
+        }
+
+        //der er ikke overlap imellem de 2 klasser, skal finde ud hvordan at jeg skal fixe det
+
+        public void AddPresentation(Presentation pre)
+        {
+            if(!Presentations.Keys.Contains(ExhibitionId))
+            {
+                Presentations.Add(PresentationId, pre);
+            }
+        }
+        public void UpdatePresentation(Presentation pre)
+        {
+            foreach (var id in Presentations)
+            {
+                if(id.Equals(PresentationId))
+                {
+                    PresentationId = PresentationId;
+                    PresentationName = PresentationName;
+                }
+            }
+        }
+
+        public void DeletePresentation(int id)
+        {
+            Presentations.Remove(id);
+        }
+
+        public Presentation readPresentation(int id)
+        {
+            return Presentations[id];
+        }
+      
+
     }
 }
