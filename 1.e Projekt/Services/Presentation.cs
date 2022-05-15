23a24af28bde197f8c20c897e347810a81bcc0e9
ericc <ericcartman2700@gmail.com>
2022-05-15 12:07:43 +0200

    Finished the database for the presentation class

--- 1.e Projekt/Services/Presentation.cs
+++ 1.e Projekt/Services/Presentation.cs
@@ -14,23 +14,24 @@ namespace _1.e_Projekt.Services
      
        public string PresentationName { get;  private set; }
        public int PresentationId { get; private set; }
+       public string ImageName { get; set; }
 
 
 
-       
+
 
         public Presentation( int ConstructorId)
             : base(ConstructorId)
        {
             Presentations = new Dictionary<int, Presentation>();
-            Presentations.Add(1, new Presentation(11) { PresentationId = 1, PresentationName = " Oplæg om Rockens opstart", ImageName = "Rock.jpg" });
-            Presentations.Add(2, new Presentation(21) { PresentationId = 1, PresentationName = " Oplæg omMusik før 2.Verdenskrig", ImageName = "Jazz.jpg" });
-            Presentations.Add(3, new Presentation(31) { PresentationId = 1, PresentationName = " Oplæg om Protestmusikken i 60'erne", ImageName = "Protest.jpg" });
-            Presentations.Add(4, new Presentation(41) { PresentationId = 1, PresentationName = " Oplæg om Stoffers indflydelse på musik", ImageName = "Woodstock.jpg" });
+            Presentations.Add(1, new Presentation(11) { PresentationId = 1, PresentationName = " Oplæg Om Rockens Opstart", ImageName = "Rock.jpg" });
+            Presentations.Add(2, new Presentation(21) { PresentationId = 1, PresentationName = " Oplæg Om Musik Før 2.Verdenskrig", ImageName = "Jazz.jpg" });
+            Presentations.Add(3, new Presentation(31) { PresentationId = 1, PresentationName = " Oplæg Om Protestmusikken i 60'erne", ImageName = "Protest.jpg" });
+            Presentations.Add(4, new Presentation(41) { PresentationId = 1, PresentationName = " Oplæg Om Stoffers Indflydelse På Musik", ImageName = "Woodstock.jpg" });
 
 
 
-
+       
 
             //foreach (var ids in Presentations.Values)
             //{
