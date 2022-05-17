1a992fe4b3a17749d1d6c747eb7cc3878149537c
ericc <ericcartman2700@gmail.com>
2022-05-17 09:21:32 +0200

    Added data annotations

--- 1.e Projekt/Services/Exhibition.cs
+++ 1.e Projekt/Services/Exhibition.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,12 +15,25 @@ namespace _1.e_Projekt.Services
        public Dictionary<int, Exhibition> Exhibitions { get; set; }
         public Dictionary<int, Exhibition> Presentations { get; set; }
         [Key]
+        [Required(ErrorMessage ="ExhibitionId is required")]
+        [DisplayName("ExhibitionId")]
+   
         public int ExhibitionId { get; set; }
+        [Required(ErrorMessage = "Exhibition Name is required")]
+        [DisplayName("Exhibition´Name")]
+        [StringLength(60, MinimumLength =3)]
+
         public string ExhibitionName { get; set; }
+        [Required(ErrorMessage = "Presentation Name  is required")]
+        [DisplayName("Presentation Name")]
+        [StringLength(60, MinimumLength = 3)]
         public string PresentationName { get; set; }
         [Key]
+        [Required(ErrorMessage = "Presentation Id is required")]
+        [DisplayName("Presentation Id")]
         public int PresentationId { get; set; }
 
+        [DisplayName("Image Name")]   
         public string ImageName { get; set; }
         //not currently in use, might use it again later
         public int CtorId { get; set; }
--- 1.e Projekt/Services/Users.cs
+++ 1.e Projekt/Services/Users.cs
@@ -1,22 +1,38 @@
 ﻿using _1.e_Projekt.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace _1.e_Projekt.Services
 {
-    public class Users: IUserInterface
+    public class Users : IUserInterface
     {
-        [Required(ErrorMessage ="you must enter something, literally anything"), MinLength(4), MaxLength(100)]
+        [Required(ErrorMessage = "you must enter something, literally anything")]
+        [DisplayName("First Name")]
+        [StringLength(40, MinimumLength = 4)]
         public string FirstName { get; set; }
-        [Required(ErrorMessage = "you must enter something, literally anything"), MinLength(4), MaxLength(100)]
+        [Required(ErrorMessage = "you must enter something, literally anything")]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "you must enter something, literally anything")]
+        [DisplayName("Email Address")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(100)]
+        //not certain that my regular expression is correct, might have to fix it later
+        [RegularExpression("^[0-9a-zA-z]+[!.?_-,]{0-1}[0-9a-zA-z]+[@][a-zA-z]+[.][a-zA-z]{2.3}[.][a-zA-z]{2.3}$")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "you must enter something, literally anything")]
+        [DataType(DataType.Password)]
+        [RegularExpression("^[0-9]{1.10}")]
         public string Password { get; set; }
         public string UserIcon { get; set; }
         [Key]
+        [DataType(DataType.Custom)]
+        [Required(ErrorMessage = "you must enter something, literally anything")]
         public int UserId { get; set; }
 
         public Dictionary<int, Users> UserCollection { get; set; }
